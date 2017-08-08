96461a2866a93122290f221b00533ea16ece477d
Rafaella <Dani1610>
2017-08-07 23:17:46 -0300

    Criando o BDConfig e a Classe Proprietario

---
+++ app/src/main/java/br/edu/iff/pooa20171/exerciciorealm/Model/BDConfig.java
@@ -0,0 +1,23 @@
+package br.edu.iff.pooa20171.exerciciorealm.Model;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+import android.content.Context;
+
+public class BDConfig extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Realm.init(getApplicationContext());
+
+        RealmConfiguration.Builder builder = new RealmConfiguration.Builder();
+        builder.name("exercicioR.realm");
+        builder.schemaVersion(0);
+        builder.deleteRealmIfMigrationNeeded();
+        RealmConfiguration realmConfiguration = builder.build();
+        Realm.setDefaultConfiguration(realmConfiguration);
+    }
+}
